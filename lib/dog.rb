a2156440aceb643a852e0949e39bc54c25cebd65
Mandy Pilaud <mandyypilaud@gmail.com>
2018-09-27 17:25:33 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,18 +1,14 @@
 class Dog
- 
-  def name =(dog_name)
+   def name=(dog_name)
     @name = dog_name
   end
-  
-  def name
+   def name
     @name
   end
-  
-  def breed =(dog_breed)
+   def breed=(dog_breed)
     @breed = dog_breed
   end
-  
-  def breed
+   def breed
     @breed
   end
-end
+ end
