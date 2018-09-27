c22bf788856010de761becc3ac3a34ab48e75889
Mandy Pilaud <mandyypilaud@gmail.com>
2018-09-27 17:23:48 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,9 @@
 class Dog
-  def initialize (dog_name, dog_breed)
-    @dog_name = dog_name
-    @dog_breed = dog_breed
-  end
-  
+ 
   def name =(dog_name)
     @name = dog_name
   end
+  
   def name
     @name
   end
